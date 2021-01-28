6636aa0556043cca6dde6e65c12e60f827c5c0e1
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-01-29 00:05:53 +0100

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -29,11 +29,7 @@ public class SimpleEmailService {
     private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
-        if(mail.getToCc() != null) {
-            mailMessage.setCc(mail.getToCc());
-        } else {
-            mailMessage.setCc("");
-        }
+        if(mail.getToCc() != null) mailMessage.setCc(mail.getToCc());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
