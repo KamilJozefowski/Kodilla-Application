3b9f0f2205cc65ed2f177ca8ea52bea21c3d08fd
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-01-28 18:34:59 +0100

    23.1. Stworzenie widoku pozwalającego na eksport zadań

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,16 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
 public class TasksApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(TasksApplication.class);
-//    }
-
 }
+
+/* Używany przy tworzenu .war dla Tomcat
+
+@SpringBootApplication
+public class TasksApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasksApplication.class);
+    }
+
+}*/
